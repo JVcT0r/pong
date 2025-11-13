28a545cc0ca2c2aa9edd90439da6f9f1a3c21c0c
João Victor M <145566469+JVcT0r@users.noreply.github.com>
2025-11-13 11:18:37 -0300

    chat de texto

---
+++ Pratica 5/Assets/Scripts/ServerChat.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using TMPro;
+
+public class TcpServerUnity : MonoBehaviour
+{
+    TcpListener server;
+    Thread serverThread;
+    public TMP_Text text;
+
+    void Start()
+    {
+        serverThread = new Thread(new ThreadStart(StartServer));
+        serverThread.IsBackground = true;
+        serverThread.Start();
+    }
+    
+    void StartServer()
+    {
+        server = new TcpListener(IPAddress.Any, 8080);
+        server.Start();
+        Debug.Log("Servidor ouvindo na porta 8080...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int len = stream.Read(buffer, 0, buffer.Length);
+            string msg = Encoding.UTF8.GetString(buffer, 0, len);
+            Debug.Log($"[Servidor] Mensagem recebida: {msg}");
+            text.text = msg;
+
+            stream.Close();
+            client.Close();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        server?.Stop();
+        serverThread?.Abort();
+    }
+}
