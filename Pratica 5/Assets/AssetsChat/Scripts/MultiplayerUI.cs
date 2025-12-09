590cf49b035330612ba38222496abbb9a5e0fe55
João Victor M <145566469+JVcT0r@users.noreply.github.com>
2025-12-09 14:38:32 -0300

    DEU ERRADO ESSA MERDA

--- Pratica 5/Assets/AssetsChat/Scripts/MultiplayerUI.cs
+++ Pratica 5/Assets/AssetsChat/Scripts/MultiplayerUI.cs
@@ -1,21 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine.UI;
+using UnityEngine;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 
 public class MultiplayerUI : MonoBehaviour
 {
-    [SerializeField] Button hostBtn, joinBtn;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void Awake()
+
+private async void Awake()
+{
+    await UnityServices.InitializeAsync();
+    await AuthenticationService.Instance.SignInAnonymouslyAsync();
+}
+
+void Start()
     {
-        AssignInputs();
+        hostButton.onClick.AddListener(HostButtonOnClick);
+        clientButton.onClick.AddListener(ClientButtonOnClick);
+        
+        
     }
 
-    void AssignInputs()
+    [SerializeField] private TextMeshProUGUI JoinCodeText;
+    private async void HostButtonOnClick()
     {
-        hostBtn.onClick.AddListener( delegate { NetworkManager.Singleton.StartHost(); } );
-        joinBtn.onClick.AddListener( delegate { NetworkManager.Singleton.StartClient(); } );
+        try
+        {
+            
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+            string JoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            JoinCodeText.text = JoinCode;
+            
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+        
+    }
+
+    [SerializeField] private TMP_InputField JoinCodeInput;
+    private async void ClientButtonOnClick()
+    {
+        try
+        {
+           JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(JoinCodeInput.text);
+            
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+        
     }
 }
